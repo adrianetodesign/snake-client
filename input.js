de365b5c03869a07437ac3f0983e7f25a0d60804
adrianetodesign <adrianetodesign@gmail.com>
2022-01-19 21:50:49 +0000

    Added WASD movement to snake client.

--- input.js
+++ input.js
@@ -1,14 +1,29 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
   return stdin;
+
 };
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  switch (key) {
+    case "\u0077" : connection.write("Move: up");
+    break;
+    case "\u0061" : connection.write("Move: left");
+    break;
+    case "\u0073" : connection.write("Move: down");
+    break;
+    case "\u0064" : connection.write("Move: right");
+    break;
   }
 };
 
